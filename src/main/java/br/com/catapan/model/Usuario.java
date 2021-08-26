a67ff0ee4190abedf4be94678b575a3f53035ea8
Danilo <danilo.foton@foton.la>
2021-08-26 17:43:21 -0300

    add UsuarioResource with post Usuario

--- src/main/java/br/com/catapan/model/Usuario.java
+++ src/main/java/br/com/catapan/model/Usuario.java
@@ -1,12 +1,14 @@
 package br.com.catapan.model;
 
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Usuario {
+public class Usuario extends PanacheEntityBase {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
