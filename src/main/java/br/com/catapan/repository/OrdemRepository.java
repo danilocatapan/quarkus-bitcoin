c1bac53722ebd177107facfe92db109c23ab5a89
Danilo <danilo.foton@foton.la>
2021-08-26 17:55:01 -0300

    add OrdemRepository with post PanacheRepository

---
+++ src/main/java/br/com/catapan/repository/OrdemRepository.java
@@ -0,0 +1,10 @@
+package br.com.catapan.repository;
+
+import br.com.catapan.model.Ordem;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class OrdemRepository implements PanacheRepository<Ordem> {
+}
