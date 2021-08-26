502693f1ab6880757cbdfc2d9c34a9ff1da7a5ff
Danilo <danilo.foton@foton.la>
2021-08-26 16:52:02 -0300

    add Bitcoin model

---
+++ src/main/java/br/com/catapan/model/Bitcoin.java
@@ -0,0 +1,46 @@
+package br.com.catapan.model;
+
+import java.time.LocalDate;
+
+public class Bitcoin {
+
+    private Long id;
+    private Double preco;
+    private String tipo;
+    private LocalDate data;
+
+    public Bitcoin() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+}
