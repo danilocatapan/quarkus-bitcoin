dd0f1329779ff788bed8222937f262df46d6cdc0
Danilo <danilo.foton@foton.la>
2021-08-26 17:26:49 -0300

    add postgres datasource into application.properties

--- src/main/java/br/com/catapan/model/Bitcoin.java
+++ src/main/java/br/com/catapan/model/Bitcoin.java
@@ -5,8 +5,11 @@ import java.time.LocalDate;
 public class Bitcoin {
 
     private Long id;
+
     private Double preco;
+
     private String tipo;
+
     private LocalDate data;
 
     public Bitcoin() {
