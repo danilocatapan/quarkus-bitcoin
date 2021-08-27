7e2d94fbf890d0d47cf357c48e1f313e47983ec6
Danilo <danilo.foton@foton.la>
2021-08-26 22:42:46 -0300

    add listAll in OrdemResource

--- src/main/java/br/com/catapan/resoure/OrdemResource.java
+++ src/main/java/br/com/catapan/resoure/OrdemResource.java
@@ -6,12 +6,11 @@ import br.com.catapan.service.OrdemService;
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.SecurityContext;
+import java.util.List;
 
 @Path("ordens")
 public class OrdemResource {
@@ -25,5 +24,12 @@ public class OrdemResource {
     @Consumes(MediaType.APPLICATION_JSON)
     public void inserir(@Context SecurityContext securityContext, Ordem ordem) {
         ordemService.inserir(securityContext, ordem);
+    }
+
+    @GET
+    @RolesAllowed("admin")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Ordem> listar() {
+        return ordemService.listar();
     }
 }
