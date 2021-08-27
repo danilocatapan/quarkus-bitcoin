92cfe57c5a3db522e9eb4fd29aadcd2888e6880b
Danilo <danilo.foton@foton.la>
2021-08-26 22:12:42 -0300

    add @PermitAll in UsuarioResource

--- src/main/java/br/com/catapan/resoure/UsuarioResource.java
+++ src/main/java/br/com/catapan/resoure/UsuarioResource.java
@@ -2,6 +2,7 @@ package br.com.catapan.resoure;
 
 import br.com.catapan.model.Usuario;
 
+import javax.annotation.security.PermitAll;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -12,9 +13,11 @@ import javax.ws.rs.core.MediaType;
 public class UsuarioResource {
 
     @POST
+    @PermitAll //permite qualquer usuario acessar
     @Transactional
     @Consumes(MediaType.APPLICATION_JSON)
     public void inserir(Usuario usuario) {
+        Usuario.adicionar(usuario);
         Usuario.persist(usuario);
     }
 }
